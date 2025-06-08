ecbb8262c6d413ceeb5eff2cb63f11ac7c1696e7
charles burton <charles.burton@cybergrx.com>
2025-06-08 18:51:24 -0400

    feat: add shared preferences check for setup

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"log/slog"
+
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
+const (
+	AppID    = "com.github.charles-d-burton.noid"
+	SetupKey = "setup"
+)
+
 func main() {
-	a := app.New()
+	a := app.NewWithID(AppID)
+	setup := a.Preferences().Bool(SetupKey)
+	if !setup {
+		slog.Info("setup has not been ran, running initial setup")
+
+		a.Preferences().SetBool(SetupKey, true)
+	}
+	if setup {
+		slog.Info("setup was ran, starting application")
+	}
+
 	w := a.NewWindow("Noid")
 	w.SetContent(widget.NewLabel("Welcome to Noid"))
 	w.ShowAndRun()
